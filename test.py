39970fc74c254da9e2dda77d7db616d2abb3cf86
ikzsl <ikzsl@yandex.ru>
2016-06-30 19:48:48 +0300

    asd

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-print("buybu")
-print("hellnmbnmoworld")
-print("dddddddddddddddddddd")
-print("buybu")
-print("hellnmbnmoworld")
+koloda = [6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(koloda)
+
+count = 0
+while True:
+    choice = input("Будете брать карту? y/n\n")
+    if choice == "y":
+        current = koloda.pop()
+        print("карта %d" %current)
+        count += current
+
+
+        
+        print(count)
